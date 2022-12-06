1244745e59b62190ec8e832267363b2884a8e148
LOZH-dot <68919220+LOZH-dot@users.noreply.github.com>
2022-12-06 15:53:52 +0300

    исправлены баги

--- Z1_Console/Program.cs
+++ Z1_Console/Program.cs
@@ -12,11 +12,6 @@ namespace Z1_Console
         {
             double x0 = 0, y0 = 0;
 
-            double x1 = 0, x2 = 0, x3 = -23;
-            double y1 = 0, y2 = 23, y3 = 23;
-
-            double a = 0, b = 0, c = 0;
-
             while (true)
             {
                 try
@@ -34,16 +29,12 @@ namespace Z1_Console
                 }
             }
 
-            a = (x1 - x0) * (y2 - y1) - (x2 - x1) * (y1 - y0);
-            b = (x2 - x0) * (y3 - y2) - (x3 - x2) * (y2 - y0);
-            c = (x3 - x0) * (y1 - y3) - (x1 - x3) * (y3 - y0);
-
-            if (a == 0 || b == 0 || c == 0)
-                Console.WriteLine("Точка находится на границе");
-            else if (a > 0 && b > 0 && c > 0)
+            if (Math.Abs(x0) < y0 && x0 < 0 && x0 > -23 && y0 < 23 && y0 > 0)
                 Console.WriteLine("Точка находится внутри");
-            else
+            else if (x0 > y0 || y0 < 0 || y0 > 23)
                 Console.WriteLine("Точка находится снаружи");
+            else
+                Console.WriteLine("Точка находится на границе");
         }
     }
 }
--- Z1_Forms/Form1.cs
+++ Z1_Forms/Form1.cs
@@ -21,11 +21,6 @@ namespace Z1_Forms
         {
             double x0 = 0, y0 = 0;
 
-            double x1 = 0, x2 = 0, x3 = -23;
-            double y1 = 0, y2 = 23, y3 = 23;
-
-            double a = 0, b = 0, c = 0;
-
             try
             {
                 x0 = double.Parse(XTextBox.Text);
@@ -38,16 +33,12 @@ namespace Z1_Forms
                 return;
             }
 
-            a = (x1 - x0) * (y2 - y1) - (x2 - x1) * (y1 - y0);
-            b = (x2 - x0) * (y3 - y2) - (x3 - x2) * (y2 - y0);
-            c = (x3 - x0) * (y1 - y3) - (x1 - x3) * (y3 - y0);
-
-            if (a == 0 || b == 0 || c == 0)
-                MessageBox.Show("Точка находится на границе", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            else if (a > 0 && b > 0 && c > 0)
+            if (Math.Abs(x0) < y0 && x0 < 0 && x0 > -23 && y0 < 23 && y0 > 0)
                 MessageBox.Show("Точка находится внутри", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            else
+            else if (x0 > y0 || y0 < 0 || y0 > 23)
                 MessageBox.Show("Точка находится снаружи", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("Точка находится на границе", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
         }
     }
